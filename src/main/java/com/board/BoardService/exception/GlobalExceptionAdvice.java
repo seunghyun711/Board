39eef0d07c18bd7a180530f05e9d3fec98fe6af5
seunghyun711 <seunghyun711@gmail.com>
2023-04-30 00:15:48 +0900

    GlobalExceptionAdvice 생성

---
+++ src/main/java/com/board/BoardService/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,34 @@
+package com.board.BoardService.exception;
+
+import com.board.BoardService.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice // Controller 클래스에서 @ExceptionHandler가 추가된 메서드를 공유해서 사용 -> 예외 처리 공통화
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+}
